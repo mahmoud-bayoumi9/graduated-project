3e709481d2d224729d118e769fa4b41ffe438228
MahmoudBaiomyNourEldeen <89879364+mahmoud-bayoumi9@users.noreply.github.com>
2026-06-26 11:29:30 -0700

    Implement setup and teardown in baseTest
    Added setup and teardown methods for GUI tests.

--- src/test/java/demoPlaze/baseTest.java
+++ src/test/java/demoPlaze/baseTest.java
@@ -3,11 +3,28 @@ package demoPlaze;
 import demoPlaze.drivers.GuiDriver;
 import demoPlaze.drivers.webDriverProvider;
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
 public class baseTest implements webDriverProvider {
     protected GuiDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        // 🚀 هنا بنخليه يفتح الـ GuiDriver اللي متظبط فيه الـ Headless والـ Options تلقائياً
+        driver = new GuiDriver();
+    }
+
     @Override
     public WebDriver getWebDriver() {
         return driver.get();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        // 🛑 قفل المتصفح وتنظيف الـ ThreadLocal بعد كل تيست كيس
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
