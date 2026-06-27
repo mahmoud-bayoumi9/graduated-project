e25acde1d6c81d7fa55dc1ec410931d48e24f694
MahmoudBaiomyNourEldeen <89879364+mahmoud-bayoumi9@users.noreply.github.com>
2026-06-27 04:54:09 -0700

    Refactor ProductsTest for clarity and structure
    Removed unnecessary test annotations and updated setup method to rely on baseTest. Adjusted test methods to include descriptions and priorities.

--- src/test/java/demoPlaze/ProductsTest.java
+++ src/test/java/demoPlaze/ProductsTest.java
@@ -1,83 +1,75 @@
 package demoPlaze;
 
 import com.google.common.collect.ImmutableMap;
-import demoPlaze.Pages.ConfirmationPage;
 import demoPlaze.Pages.ProductsPage;
-import demoPlaze.Pages.SignupOrLogin;
 import demoPlaze.Pages.commponent.NavigationBar;
 import demoPlaze.customListener.testNGListener;
-import demoPlaze.drivers.GuiDriver;
 import fakergenerate.generateConfirmationUser;
 import io.qameta.allure.Owner;
 import io.qameta.allure.testng.Tag;
-import jdk.jfr.Description;
+import io.qameta.allure.Description; // تصحيح مكتبة الـ Description لتكون تابعة لـ Allure
 import org.testng.annotations.*;
 
 import static com.github.automatedowl.tools.AllureEnvironmentWriter.allureEnvironmentWriter;
+
 @Listeners(testNGListener.class)
 public class ProductsTest extends baseTest {
-    @Test
-    @Description("User should Redirect to confirmation Page")
 
     @BeforeSuite
     void setAllureEnvironment() {
+        // 👈 شيلنا الـ @Test والـ @Description الغلط اللي كانوا فوق الدالة دي
         allureEnvironmentWriter(
                 ImmutableMap.<String, String>builder()
                         .put("Browser", "Chrome")
-                        .put("Browser.Version", "70.0.3538.77").
-                        put("os", System.getProperty("os.name"))
+                        .put("Browser.Version", "70.0.3538.77")
+                        .put("os", System.getProperty("os.name"))
                         .put("URL", "http://testjs.site88.net")
                         .build());
     }
+
     models.confirmationUser conf = generateConfirmationUser.returnedConfirmationUser();
+
     @BeforeMethod
-    public void setup(){
-        driver=new GuiDriver();
-        String email="abanob.soror@gmail.com";
+    public void setupPages(){
+        // ⚠️ مسحنا driver = new GuiDriver() الزيادة عشان نعتمد على الـ baseTest بأمان
         new NavigationBar(driver).navigate();
     }
-    @Test
-    public  void VerifyUserCanAddProductToCartSuccessfully(){
-//        TC45
+
+    @Test(priority = 1)
+    @Description("Verify User Can Add Product To Cart Successfully")
+    public void VerifyUserCanAddProductToCartSuccessfully(){
         new NavigationBar(driver).clickOnProductsButton();
-        ProductsPage product=
-        new ProductsPage(driver);
+        ProductsPage product = new ProductsPage(driver);
         product.addProduct();
         product.verifyAddedBox("Added!");
         product.continueShopping();
     }
 
-    @Test
-    public  void VerifyUserCanRemoveProductFromCartSuccessfully(){
-//        TC64
+    @Test(priority = 2)
+    @Description("Verify User Can Remove Product From Cart Successfully")
+    public void VerifyUserCanRemoveProductFromCartSuccessfully(){
         new NavigationBar(driver).clickOnProductsButton();
-        ProductsPage product=
-                new ProductsPage(driver);
+        ProductsPage product = new ProductsPage(driver);
         product.viewCart().deleteProduct();
     }
 
-    @Test
-    public  void VerifySearchFunctionalityUsingTShirtKeyword(){
-//        TC47
+    @Test(priority = 3)
+    @Description("Verify Search Functionality Using TShirt Keyword")
+    public void VerifySearchFunctionalityUsingTShirtKeyword(){
         new NavigationBar(driver).clickOnProductsButton();
-        ProductsPage product=
-                new ProductsPage(driver);
+        ProductsPage product = new ProductsPage(driver);
         product.search("T-Shirts");
         product.searchButton();
         product.verifySearchProduct("Premium Polo T-Shirts");
     }
 
-    @Test
-    public  void  VerifyUserCanAddMultipleQuantitiesOfTheSameProductToCart(){
-//        TC48
+    @Test(priority = 4)
+    @Description("Verify User Can Add Multiple Quantities Of The Same Product To Cart")
+    public void VerifyUserCanAddMultipleQuantitiesOfTheSameProductToCart(){
         new NavigationBar(driver).clickOnProductsButton();
-        ProductsPage product=
-                new ProductsPage(driver);
+        ProductsPage product = new ProductsPage(driver);
         product.productDetails().addQuantityOfSameProduct("4");
     }
-    @AfterMethod
-    public void tearDown(){
-        driver.quit();
-    }
-}
 
+    // ⚠️ تم حذف دالة الـ tearDown() لأن الـ baseTest بيقوم بالدور ده بدلاً عنها منعاً للـ الـ Conflict
+}
