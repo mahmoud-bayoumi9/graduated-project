56ea9f4419d8ce57958f83fab6cc2805390a9f68
MahmoudBaiomyNourEldeen <89879364+mahmoud-bayoumi9@users.noreply.github.com>
2026-06-27 05:22:41 -0700

    Update deleteAccountTest.java

--- src/test/java/demoPlaze/deleteAccountTest.java
+++ src/test/java/demoPlaze/deleteAccountTest.java
@@ -6,78 +6,65 @@ import demoPlaze.Pages.SignupOrLogin;
 import demoPlaze.Pages.commponent.NavigationBar;
 import demoPlaze.Pages.deleteAccountPage;
 import demoPlaze.customListener.testNGListener;
-import demoPlaze.drivers.GuiDriver;
 import fakergenerate.generateConfirmationUser;
 import io.qameta.allure.Owner;
 import io.qameta.allure.testng.Tag;
-import jdk.jfr.Description;
+import io.qameta.allure.Description; // ✅ تصحيح الـ Import لـ Allure
 import org.testng.annotations.*;
-
-// import static com.github.automatedowl.tools.AllureEnvironmentWriter.allureEnvironmentWriter;
 
 @Listeners(testNGListener.class)
 public class deleteAccountTest extends baseTest {
-    @Test
-    public class deleteAccount extends baseTest {
-        @Test(description = "User should Redirect to confirmation Page")
 
-        @Tag("userDetails")
-        @Owner("Abanob")
+    @BeforeSuite
+    void setAllureEnvironment() {
+        com.github.automatedowl.tools.AllureEnvironmentWriter.allureEnvironmentWriter(
+                com.google.common.collect.ImmutableMap.<String, String>builder()
+                        .put("Browser", "Chrome")
+                        .put("Browser.Version", "70.0.3538.77")
+                        .put("os", System.getProperty("os.name"))
+                        .put("URL", "http://testjs.site88.net")
+                        .build());
+    }
 
-       @BeforeSuite
-void setAllureEnvironment() {
-  
-    com.github.automatedowl.tools.AllureEnvironmentWriter.allureEnvironmentWriter(
-            com.google.common.collect.ImmutableMap.<String, String>builder()
-                    .put("Browser", "Chrome")
-                    .put("Browser.Version", "70.0.3538.77")
-                    .put("os", System.getProperty("os.name"))
-                    .put("URL", "http://testjs.site88.net")
-                    .build());
-}
+    models.confirmationUser conf = generateConfirmationUser.returnedConfirmationUser();
 
-        models.confirmationUser conf = generateConfirmationUser.returnedConfirmationUser();
+    @BeforeMethod
+    public void setup() {
+        // 🚀 بنعتمد على الـ Driver اللي بيتحضر تلقائياً وبأمان في الـ baseTest
+        new NavigationBar(getWebDriver()).navigate();
+        SignupOrLogin auth = new SignupOrLogin(getWebDriver());
+        auth.signupOrLogin()
+                .EnterRegisterName(conf.getName())
+                .EnterRegisterEmail(conf.getEmail())
+                .clickRegisterButton();
+                
+        new ConfirmationPage(getWebDriver()).fillRegisterForm(
+                conf.getTitle(), conf.getPassword(), conf.getDay(), conf.getMonth(), conf.getYear(), 
+                conf.getFirstName(), conf.getLastName(), conf.getCompany(), conf.getAddress(), 
+                conf.getAddress2(), conf.getCountry(), conf.getState(), conf.getCity(), 
+                conf.getZipCode(), conf.getMobile()).clickCreate().verifyContenueButton();
+    }
 
-        @BeforeMethod
-        public void setup() {
-            driver = new GuiDriver();
-            String email = "abanob.soror@gmail.com";
-            new NavigationBar(driver).navigate();
-            SignupOrLogin auth = new SignupOrLogin(driver);
-            auth.signupOrLogin()
-                    .EnterRegisterName(conf.getName()).EnterRegisterEmail(conf.getEmail()).clickRegisterButton();
-            new ConfirmationPage(driver).fillRegisterForm(conf.getTitle(), conf.getPassword(), conf.getDay(), conf.getMonth(), conf.getYear(), conf.getFirstName(), conf.getLastName(), conf.getCompany(), conf.getAddress(), conf.
-                            getAddress2(), conf.getCountry(),
-                    conf.getState(), conf.getCity(), conf.getZipCode(), conf.getMobile()).clickCreate().verifyContenueButton();
-//            String actualUrl=driver.get().getCurrentUrl();
-//            auth.signupOrLogin().EnterLoginEmail(conf.getEmail()).EnterLoginPassword(conf.getPassword()).clickLoginButton();
-        }
+    @Test(priority = 1)
+    @Description("User should able to delete account")
+    @Tag("userDetails")
+    @Owner("Abanob")
+    public void VerifyUserCanDeleteAccountSuccessfully() {
+        deleteAccountPage delete = new deleteAccountPage(getWebDriver());
+        delete.deleteAccount();
+        delete.verifyVisibleDeletedParagraph();
+    }
 
-        @Description("User should able to delete account")
-        @Test
-        public void VerifyUserCanDeleteAccountSuccessfully() {
-//        TC41
-            deleteAccountPage delete = new deleteAccountPage(driver);
-            delete.deleteAccount();
-            delete.verifyVisibleDeletedParagraph();
-        }
-
-
-        @Description("User should able to logout and login again successfully")
-        @Test
-        public void VerifyUserCanLogoutAndLoginAgainSuccessfully() {
-//            TC42
-            NavigationBar navigate =
-                    new NavigationBar(driver);
-            navigate.clickOnLogOutButton().EnterLoginEmail(conf.getEmail()).EnterLoginPassword(conf.getPassword()).clickLoginButton();
-            navigate.verifyLoginAs(conf.getName());
-
-        }
-
-        @AfterMethod
-        public void tearDown() {
-            driver.quit();
-        }
-
+    @Test(priority = 2)
+    @Description("User should able to logout and login again successfully")
+    @Tag("userDetails")
+    @Owner("Abanob")
+    public void VerifyUserCanLogoutAndLoginAgainSuccessfully() {
+        NavigationBar navigate = new NavigationBar(getWebDriver());
+        navigate.clickOnLogOutButton()
+                .EnterLoginEmail(conf.getEmail())
+                .EnterLoginPassword(conf.getPassword())
+                .clickLoginButton();
+        navigate.verifyLoginAs(conf.getName());
     }
 }
