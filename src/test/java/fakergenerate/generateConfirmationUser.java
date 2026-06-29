3355cb1406894dab385dd59a595e805009828260
MahmoudBaiomyNourEldeen <89879364+mahmoud-bayoumi9@users.noreply.github.com>
2026-06-29 02:27:21 -0700

    Update generateConfirmationUser.java

--- src/test/java/fakergenerate/generateConfirmationUser.java
+++ src/test/java/fakergenerate/generateConfirmationUser.java
@@ -9,8 +9,6 @@ public class generateConfirmationUser {
     public static confirmationUser returnedConfirmationUser() {
         Faker faker = new Faker(new Locale("en-US"));
         confirmationUser conf = new confirmationUser();
-
-        // 🎯 توليد البيانات العادية بالـ Faker
         conf.setName(faker.name().username());
         conf.setEmail("mahmoud" + System.currentTimeMillis() + "@test.com");
         conf.setPassword(faker.internet().password());
@@ -24,13 +22,11 @@ public class generateConfirmationUser {
         conf.setCity(faker.address().city());
         conf.setZipCode(faker.address().zipCode());
         conf.setMobile(faker.phoneNumber().cellPhone());
-
-        // 🚀 تثبيت قيم الـ Dropdowns لضمان النجاح على الـ Pipeline وتجنب الـ Redirect Failure
         conf.setDay("10");
         conf.setMonth("May");
         conf.setYear("2000");
         conf.setCountry("United States");
 
         return conf;
-    } // 👈 قفلة الدالة (returnedConfirmationUser)
-} // 👈 قفلة الكلاس (generateConfirmationUser)
+    } 
+} 
